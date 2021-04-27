d9ce78a5988c9090df9d7709cebeb9cf52b5acdf
Clemens Mauksch <56268428+clemensma@users.noreply.github.com>
2021-04-27 16:17:22 +0200

    PEP8 conformities established

--- bin/coverage_estimate.py
+++ bin/coverage_estimate.py
@@ -1,21 +1,25 @@
-alignment_rate = open('alignment_rate.txt', 'r')               # Get alignment rate from bowtie2 output
-line = alignment_rate.readlines()[-1]
-rate = ''
-for i in line:
-    if i == '%': break
-    rate += i
-rate = float(rate)/100
-alignment_rate.close()
+def main():
+    
+    with open('alignment_rate.txt', 'r') as alignment_rate_file:                # Get alignment rate from bowtie2 output
+        line = alignment_rate_file.readlines()[-1]
+    alignment_rate = ''
+    for i in line:
+        if i == '%': break
+        alignment_rate += i
+    alignment_rate = float(alignment_rate)/100
 
-host_count = open('host_count.txt', 'r')            # Get no. of bases in reference genome
-count = int(host_count.readline())
-host_count.close()
+    with open('host_count.txt', 'r') as host_count:                             # Get no. of bases in reference genome
+        reference_base_count = int(host_count.readline())
 
-base_count = open('base_count.txt', 'r')            # Get no. of bases in read files
-bases = int(base_count.readline()) * 2
 
-coverage = (rate * bases) / count                   # Calculate coverage
+    with open('base_count.txt', 'r') as base_count:                             # Get no. of bases in read files
+        reads_base_count = int(base_count.readline()) * 2
 
-coverage_file = open('coverage.txt', 'w')
-coverage_file.write(str(coverage))
-coverage_file.close
+    coverage = (alignment_rate * reads_base_count) / reference_base_count       # Calculate coverage
+
+    with open('coverage.txt', 'w') as coverage_file:
+        coverage_file.write(str(coverage))
+
+    
+if __name__ == "__main__":
+    main()
